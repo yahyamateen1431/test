c58da30c17b9bb28e0e1aa5b2b4f8a8719fa6b5b
Yahya Mateen <yahyamateen1431@gmail.com>
2021-09-19 03:42:09 +0500

    second commit

--- my_site/urls.py
+++ my_site/urls.py
@@ -1,4 +1,4 @@
-"""test_project URL Configuration
+"""my_site URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
